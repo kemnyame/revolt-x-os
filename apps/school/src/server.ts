50e5fa4d23387afc2b381e9619ac32d25b32d776
kemnyame <51462465+kemnyame@users.noreply.github.com>
2026-09-20 01:48:40 +0000

    Connect EOD accounting repair services

--- apps/school/src/server.ts
+++ apps/school/src/server.ts
@@ -5404,7 +5404,7 @@ await registerFinanceLeaveRoutes(app,{
 });
 
 await registerAccountingRoutes(app,{
-  db,config,authorize,one,maybeOne,tx,fail,audit,postFinanceJournal,updateStudentFeeStatus,reverseFinanceJournal
+  db,config,authorize,one,maybeOne,tx,fail,audit,postFinanceJournal,updateStudentFeeStatus,postStudentPaymentLedger,postStudentFeeReceivable,reverseFinanceJournal
 });
 
 app.setErrorHandler(async(error:any,request,reply)=>{
